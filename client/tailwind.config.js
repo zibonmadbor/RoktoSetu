d9b72803d255dfbd1625e99fd78bb29cf3df9c5a
zibonmadbor <zibonmadbor@gmail.com>
2026-07-25 22:27:42 +0600

    feat(client): scaffold Vite React application with Tailwind CSS configuration

---
+++ client/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#fff1f2',
+          100: '#ffe4e6',
+          200: '#fecdd3',
+          500: '#f43f5e',
+          600: '#e11d48',
+          700: '#be123c',
+          800: '#9f1239',
+          900: '#881337',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
